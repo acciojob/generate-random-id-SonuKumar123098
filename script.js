8758540a9b044a2d9fbd66b3f65c78fa81714048
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-02-02 14:36:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function makeid(length) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	let result='';
 	for (let i = 0; i < length; i++) {
     // Generate a random ASCII code for alphanumeric characters (48-57: 0-9, 65-90: A-Z, 97-122: a-z)
-    const randomCharCode = Math.floor(Math.random() * (57 - 48 + 1 + 90 - 65 + 122 - 97 + 1)) + 48;
-
+    // const randomCharCode = Math.floor(Math.random() * (57 - 48 + 1 + 90 - 65 + 122 - 97 + 1)) + 48;
+		let index=Math.floor(Math.random()*characters.length);
     // Convert ASCII code to character and append to result
-    result += String.fromCharCode(randomCharCode);
+    // result += String.fromCharCode(randomCharCode);
+		result+=characters.charAt(index);
   }
 	return result;
 }
